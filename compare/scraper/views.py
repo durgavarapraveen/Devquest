5439256f742077ff3e0e59d0554b05a153f04673
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2024-01-20 02:59:35 +0530

    added backend

--- compare/scraper/views.py
+++ compare/scraper/views.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
 
 class ProductInfoView(APIView):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (AllowAny, )
 
 
     def get(self, request, searchsentence):
--- dq/Scripts/activate.bat
+++ dq/Scripts/activate.bat
@@ -8,11 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-<<<<<<< HEAD
-set VIRTUAL_ENV=C:\IITJ\Prometeo'24\Devquest\devquest\dq
-=======
 set VIRTUAL_ENV=C:\Users\G D V Praveen\OneDrive\Desktop\ReactProjects\DevQuest\dq
->>>>>>> main
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -29,10 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-<<<<<<< HEAD
-=======
 set VIRTUAL_ENV_PROMPT=(dq) 
->>>>>>> main
 
 :END
 if defined _OLD_CODEPAGE (
